9affa41eec6eb30dba8d58c7e894b2a8c07bd340
21MH1A0579 <21mh1a0579@acoe.edu.in>
2025-02-07 13:58:27 +0530

    created login page and removed unnecessary imports in home controller

--- src/main/java/com/example/demo/Controllers/HomeController.java
+++ src/main/java/com/example/demo/Controllers/HomeController.java
@@ -1,11 +1,4 @@
 package com.example.demo.Controllers;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-package net.javaguides.springboot.controller;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
