81ed88ad294da035ba9f244698f769b66f194861
21MH1A0579 <21mh1a0579@acoe.edu.in>
2025-02-07 13:50:50 +0530

    add login request in home controller

--- src/main/java/com/example/demo/Controllers/HomeController.java
+++ src/main/java/com/example/demo/Controllers/HomeController.java
@@ -4,12 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class HomeController {
-	
-	@GetMapping("/home")
-	public String home() {
-		return "welcome it's Working";
-	}
+package net.javaguides.springboot.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
 }
